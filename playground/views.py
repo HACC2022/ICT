2e58629465391740d74ac30c94ec4abab07894e6
zeek granston <zeekgranston@zeeks-MacBook-Pro-2.local>
2022-10-25 12:44:54 -1000

    inital push

---
+++ playground/views.py
@@ -0,0 +1,7 @@
+import re
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def hello(request):
+    return render (request, 'index.html', {'name': 'zeek'})
