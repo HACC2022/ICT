7e577c503bb87eea4667260d7bbe8a42849d4d00
Marvin Bock - I20 <marvin.bock@nordakademie.de>
2022-10-27 12:19:03 -1000

    Created url model and made index.html main page

--- playground/models.py
+++ playground/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Url(models.Model):
+    longLink = models.CharField(max_length=50)
+    shortCode = models.CharField(max_length=30)
+    user = models.CharField(max_length=30)
--- playground/urls.py
+++ playground/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 #URL Config
 urlpatterns = [
-    path('hello/', views.hello)
+    path('', views.hello)
 ]
