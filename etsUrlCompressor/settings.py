d5bb7392a901c16c329d751da056883db0da3916
Pete Chee Zachraias <pzachraias1@my.hpu.edu>
2022-10-26 20:54:33 -1000

    fix error. I did not add, commit, and push db.sqlite3, since I ran python3 manage.py migrate

--- etsUrlCompressor/settings.py
+++ etsUrlCompressor/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'storefront.urls'
+ROOT_URLCONF = 'etsUrlCompressor.urls'
 
 TEMPLATES = [
     {
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'storefront.wsgi.application'
+WSGI_APPLICATION = 'etsUrlCompressor.wsgi.application'
 
 
 # Database
