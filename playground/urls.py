aa361b41a8d344b037117083cf668c3a4b047dde
zeekg9335 <zeekg9335@aol.com>
2022-10-28 10:56:17 -1000

    adding login/logout and navbar to main

--- playground/urls.py
+++ playground/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 #URL Config
 urlpatterns = [
-    path('', views.hello),
+    path('', views.hello, name='index'),
     path('shorten', views.shorten, name='shorten'),
-    path('<str:pk>', views.forward, name='forward')
+    path('<str:pk>', views.forward, name='forward'),
+    path('login/', views.loginPage, name='login'),
+    path('logout/', views.logoutUser, name='logoutUser')
 ]
--- playground/views.py
+++ playground/views.py
@@ -3,8 +3,13 @@ from django.shortcuts import render, redirect
 import uuid
 from .models import Url
 from django.http import HttpResponse
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+@login_required(login_url= 'login')
 def hello(request):
     return render (request, 'index.html', {'name': 'zeek'})
 
@@ -19,3 +24,26 @@ def shorten(request):
 def forward(request, pk):
     long_url = Url.objects.get(shortCode=pk)
     return redirect(long_url.longLink)
+
+
+def loginPage(request):
+   
+    if request.method == 'POST':
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            messages.info(request,'Username or Password incorrect')
+            return redirect('login')
+    context={}
+    return render (request, 'login.html', context)
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
