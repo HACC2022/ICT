1e2e6073e15fcf3718549da3631eefef25be59ea
Pete Chee Zachraias <pzachraias1@my.hpu.edu>
2022-10-30 20:26:23 -1000

    deploying

---
+++ Procfile
@@ -0,0 +1,1 @@
+web gunicorn icturl.wsgi:application --log-file -
--- etsUrlCompressor/settings.py
+++ etsUrlCompressor/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
 from pathlib import Path
+import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +27,7 @@ SECRET_KEY = 'django-insecure-mpi%8sg9jbko#qv744mri0_+r4@8wnt)6v+9c=gut7ir+!vt5g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -117,8 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
+django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
