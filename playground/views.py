df21a580d6215b961e5dde4692b53b67dbaeb0e8
Pete Chee Zachraias <pzachraias1@my.hpu.edu>
2022-11-04 19:21:46 -1000

    made homepage look pretty as hell

--- playground/views.py
+++ playground/views.py
@@ -1,4 +1,5 @@
 import re
+import requests
 from django.shortcuts import get_object_or_404, render, redirect
 import uuid
 
@@ -13,18 +14,22 @@ from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
-@login_required(login_url= 'login')
+
+@login_required(login_url='login')
 def hello(request):
-    return render (request, 'index.html', {'name': 'zeek'})
-@login_required(login_url= 'login')
+    return render(request, 'index.html', {'name': 'zeek'})
+
+
+@login_required(login_url='login')
 def shorten(request):
     if request.method == 'POST':
         lURL = request.POST['link']
         sCode = str(uuid.uuid4())[:5]
-        shortUrl = Url(longLink=lURL,shortCode=sCode)
+        shortUrl = Url(longLink=lURL, shortCode=sCode)
         shortUrl.save()
         return HttpResponse(sCode)
-@login_required(login_url= 'login')
+
+
 def forward(request, pk):
     long_url = Url.objects.get(shortCode=pk)
     long_url.clicks += 1
@@ -33,14 +38,18 @@ def forward(request, pk):
     ip_origin = IP_Adresses(shortCode=long_url, ip_address=ip)
     ip_origin.save()
     return redirect(long_url.longLink)
-@login_required(login_url= 'login')
+
+
+@login_required(login_url='login')
 def manage_view(request):
     queryset = Url.objects.all()
     context = {
         'object_list': queryset
     }
-    return render (request, 'manage.html', context)
-@login_required(login_url= 'login')
+    return render(request, 'manage.html', context)
+
+
+@login_required(login_url='login')
 def search_view(request):
     if request.method == 'POST':
         word = request.POST['keyword']
@@ -50,22 +59,25 @@ def search_view(request):
         }
         html = render_to_string('search_result.html', context)
         return JsonResponse(html, safe=False)
-        
+
+
 def is_ajax(request):
     return request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest'
-@login_required(login_url= 'login')
+
+
+@login_required(login_url='login')
 def manage_view_delete(request, pk, *args, **kwargs):
     if is_ajax(request):
         obj = Url.objects.get(pk=pk)
         obj.delete()
-        return JsonResponse({"message":"success"})
-    return JsonResponse({"message":"Something is wrong"})
+        return JsonResponse({"message": "success"})
+    return JsonResponse({"message": "Something is wrong"})
+
 
 def loginPage(request):
-   
     if request.method == 'POST':
-        username=request.POST.get('username')
-        password=request.POST.get('password')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
 
         user = authenticate(request, username=username, password=password)
 
@@ -73,15 +85,17 @@ def loginPage(request):
             login(request, user)
             return redirect('index')
         else:
-            messages.info(request,'Username or Password incorrect')
+            messages.info(request, 'Username or Password incorrect')
             return redirect('login')
-    context={}
-    return render (request, 'login.html', context)
+    context = {}
+    return render(request, 'login.html', context)
 
-@login_required(login_url= 'login')
+
+@login_required(login_url='login')
 def logoutUser(request):
     logout(request)
     return redirect('login')
+
 
 def get_client_ip(request):
     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
