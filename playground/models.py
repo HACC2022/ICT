7d696307d540375d267ed6618c2c9f2d234704b2
Marvin Bock - I20 <marvin.bock@nordakademie.de>
2022-11-17 01:53:27 -1000

    Added password redirect feature

--- playground/models.py
+++ playground/models.py
@@ -8,7 +8,12 @@ class Url(models.Model):
     creationDate = models.DateField(default=date.today)
     clicks = models.IntegerField(default=0)
     status = models.CharField(max_length=10, default="Pending")
+    verification = models.BooleanField(default=False)
 
 class IP_Adresses(models.Model):
     shortCode = models.ForeignKey(Url, on_delete=models.CASCADE)
     ip_address = models.GenericIPAddressField(default='192.168.0.1')
+
+class Verification_Table(models.Model):
+    shortCode = models.ForeignKey(Url, on_delete=models.CASCADE)
+    password = models.CharField(max_length=100)
--- playground/views.py
+++ playground/views.py
@@ -7,7 +7,7 @@ import requests
 import pkg_resources
 from django.template.loader import render_to_string
 
-from .models import Url, IP_Adresses
+from .models import Url, IP_Adresses, Verification_Table
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
@@ -25,15 +25,26 @@ def hello(request):
 def shorten(request):
     if request.method == 'POST':
         lURL = request.POST['link']
+        pw = request.POST['pass']
         if ".gov" not in lURL:
             return HttpResponse("error")
         sCode = str(uuid.uuid4())[:5]
         shortUrl = Url(longLink=lURL, shortCode=sCode)
         shortUrl.save()
+        if pw != "":
+            veriObj = Verification_Table(shortCode=shortUrl, password=pw)
+            shortUrl.verification = True
+            shortUrl.save()
+            veriObj.save()
         return HttpResponse(settings.HOSTNAME + "/" + sCode)
 
 def forward(request, pk):
     long_url = Url.objects.get(shortCode=pk)
+    if long_url.verification is True:
+        context = {
+            'pk': pk
+        }
+        return render(request, 'redirect_verification.html', context)
     long_url.clicks += 1
     long_url.save()
     ip = get_client_ip(request)
@@ -125,3 +136,18 @@ def get_status(request, pk):
         longUrl.status = "Bad"
         longUrl.save()
         return HttpResponseRedirect(reverse('manage'))
+
+def verification(request):
+    pk = request.POST['shortcode']
+    pw = request.POST['password']
+    linkObj = Url.objects.get(shortCode=pk)
+    veriPw = (Verification_Table.objects.get(shortCode=linkObj)).password
+    if pw == veriPw:
+        linkObj.clicks += 1
+        ip = get_client_ip(request)
+        ip_origin = IP_Adresses(shortCode=linkObj, ip_address=ip)
+        linkObj.save()
+        ip_origin.save()
+        return HttpResponse(linkObj.longLink)
+    else:
+        return HttpResponse("wrong")
